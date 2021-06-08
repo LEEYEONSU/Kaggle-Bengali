f0ac66e99229e9a378fdeef8d8e9b66ab9f22445
root <yeonsulee530@gmail.com>
2021-06-08 09:11:53 +0000

    [EDIT] dataset init file

--- trainer/dataset/__init__.py
+++ trainer/dataset/__init__.py
@@ -4,36 +4,34 @@ import torchvision.transforms as transforms
 import os 
 
 # dataloader sampler
-def create(config, world_size=1, local_rank=-1, mode='train'):
-    
+def create(config, dataset, world_size=1, local_rank=-1, mode='train'):
+
     params = config[mode]
+    transformers = transforms.Compose([preprocess(t) for t in params['preprocess']] )
 
-    if config['type'] == 'cifar10':
-        transformers = transforms.Compose([preprocess(t) for t in params['preprocess']] )
-        
-        dataset = torchvision.datasets.CIFAR10(root='/root', download=True, transform=transformers, train = params.get('train', False))
+    if config['type'] == 'bengali':
+        dataset = dataset[mode]
 
-        if local_rank >= 0:
-            sampler = torch.utils.data.distributed.DistributedSampler(
-                dataset,
-                num_replicas=world_size, 
-                rank=local_rank, 
-                shuffle=params.get('shuffle', False)
-            )
-        else:
-            sampler = None
-
-        dataloader = torch.utils.data.DataLoader(
+    if local_rank >= 0:
+        sampler = torch.utils.data.distributed.DistributedSampler(
             dataset,
-            batch_size=params['batch_size'],
-            num_workers=os.cpu_count(),
-            pin_memory=True,
-            drop_last=params.get('drop_last', False),
-            sampler=sampler
+            num_replicas=world_size, 
+            rank=local_rank, 
+            shuffle=params.get('shuffle', False)
         )
-        return dataloader, sampler
     else:
-        raise AttributeError(f'not support dataset config: {config}')
+        sampler = None
+
+    dataloader = torch.utils.data.DataLoader(
+        dataset,
+        batch_size=params['batch_size'],
+        num_workers=os.cpu_count(),
+        pin_memory=True,
+        drop_last=params.get('drop_last', False),
+        sampler=sampler
+    )
+    return dataloader, sampler
+
 
 def preprocess(config):
     if config['type'] == 'pad':
