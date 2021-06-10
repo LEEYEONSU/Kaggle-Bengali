9b05fb1ebe680bf1b31d6becde20f99078c10be6
root <yeonsulee530@gmail.com>
2021-06-10 04:35:44 +0000

    [EDIT] config & configs -> configs

--- trainer/model/__init__.py
+++ trainer/model/__init__.py
@@ -5,15 +5,14 @@ import timm
 from .resnet import resnet32
 from .model_wrapper import model_wrapper
 
-def create(config):
-    if config['type'] == 'R50x1':
+def create(configs):
+    if configs['type'] == 'R50x1':
         return models.resnet50(pretrained=False)
-
     # resnet32 implementation test
-    elif config['type'] == 'resnet32':
+    elif configs['type'] == 'resnet32':
         return resnet32()
     else:
         try:
             return model_wrapper(configs)
         except:
-            raise AttributeError(f'not support architecture config: {config}')
+            raise AttributeError(f'not support architecture config: {configs}')
