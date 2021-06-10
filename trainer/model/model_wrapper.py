9cf6cb2de9b29ad8969bd1ee778eadd13d739518
root <yeonsulee530@gmail.com>
2021-06-10 04:35:04 +0000

    [EDIT] change List to ModuleList

--- trainer/model/model_wrapper.py
+++ trainer/model/model_wrapper.py
@@ -1,22 +1,24 @@
 import timm
+import torch
 import torch.nn as nn
-
+import numpy as np
 
 class model_wrapper(nn.Module):
     def __init__(self, configs):
         super(model_wrapper, self).__init__()
 
-        self.model = timm.create_model(config['type'], pretrained=config.get('pretrained', False))
+        self.model = timm.create_model(configs['type'], pretrained=configs.get('pretrained', False))
         self.features = self.model.fc.in_features
         self.model.fc = nn.Identity()
-        self._fc = [nn.Linear(self.features, num_class) for num_class in config['num_classes']]
-
+        self._fc = nn.ModuleList([nn.Linear(self.features, num_class) for num_class in configs['num_classes']])
 
     def forward(self, x):
         output = self.model(x)
-        outputs = [output for output in self._fc(output)]
+        outputs = [m(output) for m in self._fc]
 
-        return output
+        return outputs
+
+
 
 
 
